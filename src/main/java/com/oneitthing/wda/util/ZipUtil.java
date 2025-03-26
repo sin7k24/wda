db290c30b51b3492f25a4e03de4d360dc99d3002
sin7k24 <83158436+sin7k24@users.noreply.github.com>
2025-03-26 23:29:38 +0900

    tweaks

--- src/main/java/com/oneitthing/wda/util/ZipUtil.java
+++ src/main/java/com/oneitthing/wda/util/ZipUtil.java
@@ -23,9 +23,6 @@ public class ZipUtil {
 	
 	public static void unzip(File archive, File toDir, String unzippedName) throws IOException {
 
-		// ZipFileオープン
-		// 文字コードはシステムデフォルトの文字コードにしておきます
-		// Windows7だとMS932でした
 		ZipFile zipFile = new ZipFile(archive,
 				System.getProperty("file.encoding"));
 		// エントリ取得
@@ -34,31 +31,22 @@ public class ZipUtil {
 
 		String baseDir = toDir.getAbsolutePath() + File.separator + unzippedName;
 
-		// エントリの数だけループ
 		while (entries.hasMoreElements()) {
-			// ZipArchiveEntryを取得
 			ZipArchiveEntry ze = entries.nextElement();
-			// ディレクトリは処理しません
 			if (ze.isDirectory())
 				continue;
-			// ベースディレクトリ＋ZipArchiveEntry.getNameで出力パスを作ります
 			File f = new File(baseDir + File.separator + ze.getName());
-			// 親ディレクトリを取得
 			File p = f.getParentFile();
-			// ディレクトリが存在しない場合、ディレクトリを作成します
 			if (!p.exists()) {
 				p.mkdirs();
 			}
 
-			// エントリからInputStreamを取得
 			InputStream is = zipFile.getInputStream(ze);
 			BufferedInputStream bis = new BufferedInputStream(is);
 
-			// 出力用のファイルストリーム作成
 			FileOutputStream fo = new FileOutputStream(f);
 			BufferedOutputStream bo = new BufferedOutputStream(fo);
 
-			// InputStreamから読みだした分を順次ファイルに出力
 			int size = 0;
 			while ((size = bis.read(b)) > 0) {
 				bo.write(b, 0, size);
